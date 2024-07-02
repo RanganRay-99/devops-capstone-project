0041548881c298c1e5df262d6099440eb08030d2
RanganRay-99 <rangan.ray2021@vitstudent.ac.in>
2024-07-02 15:44:27 +0530

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -8,7 +8,8 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
-
+from flask_talisman import Talisman
+from flask_cors import CORS
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
@@ -16,7 +17,6 @@ app.config.from_object(config)
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
-
 # pylint: disable=wrong-import-position
 from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
@@ -24,7 +24,7 @@ from service.common import error_handlers, cli_commands  # noqa: F401 E402
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info("  ACCOUNT SERVICE   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
 
 try:
@@ -35,3 +35,6 @@ except Exception as error:  # pylint: disable=broad-except
     sys.exit(4)
 
 app.logger.info("Service initialized!")
+# create an instance of the Talisman class
+talisman = Talisman(app)
+CORS(app)
